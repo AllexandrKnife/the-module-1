8abf0739035bf096aba90d34b113a65b4a736148
AllexandrKnife <kolchin.de@gmail.com>
2024-09-22 21:40:43 +0300

    Add files via upload

---
+++ Homework #2.py
@@ -0,0 +1,28 @@
+repeat = input(
+    '\nДля рассчета среднего времени выполнения домашнего задания нажмите "y", или любую другую клавишу для выхода: \n')
+while repeat.lower() == 'y':
+    completed_homework = input('\nВведите количество выполненных домашних заданий в виде целого числа: \n')
+    while not completed_homework.isdigit():
+        print('\nВведите количество выполненных домашних заданий в виде целого числа: \n')
+        completed_homework = input()
+    completed_homework = int(completed_homework)
+
+    hours_spent = input('\nВведите количество затраченных часов: \n')
+    while True:
+        try:
+            hours_spent = float(hours_spent)
+            break
+        except ValueError:
+            print('\nВведите количество затраченных часов в виде дробного числа: \n')
+            hours_spent = input()
+
+    course_name = input('\nВведите название курса: \n')
+    while not course_name:
+        print('\nВведите название курса: \n')
+        course_name = input()
+
+    time_per_assignment = hours_spent / completed_homework
+    print(
+        f"Курс: {course_name}, всего выполнено домашних заданий: {completed_homework}, затрачено часов: {hours_spent}, среднее время выполнения: {time_per_assignment:.3f} часа.")
+
+    repeat = input('\nДля повтора нажмите "y", или любую другую клавишу для выхода: \n')
